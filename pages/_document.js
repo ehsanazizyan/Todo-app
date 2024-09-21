dfc155596b25209d463c38c6718823c4064a9edc
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-09-21 18:42:36 +0330

    feat: add meta description and viewport settings to the Document file
    - Added meta charSet for UTF-8 support.
    - Added meta viewport for device compatibility.
    - Added meta description to improve SEO.

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                <meta name="description" content="Todo App - Manage your tasks efficiently" />
+            </Head>
             <body>
                 <Main />
 
