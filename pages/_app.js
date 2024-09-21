ced8ea812314b2f49323b14111d2c4e2e824221e
Ehsan Azizyan <ehsanazizyan74@gmail.com>
2024-09-21 18:41:25 +0330

    feat: add meta title, description, and favicon to improve Todo App's SEO
    - Added title and meta description elements for SEO purposes.
    - Set favicon for Todo App pages.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import TopBar from "@/TopBar";
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <title>Todo App</title>
+                <meta name="description" content="Todo App to manage your tasks efficiently" />
+                <link rel="icon" href="/favicon.png" />
+            </Head>
             <TopBar />
             <Component {...pageProps} />
         </>
